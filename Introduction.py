364378f5d8cbebd0ed47d5922a57d8bb8d2597f1
kimannii00 <samanthakimani06@gmail.com>
2025-02-03 21:27:11 +0300

    initial commit

---
+++ Introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display values in a console
+
+print("Samantha")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ Variable.py
@@ -0,0 +1,15 @@
+#Variable is a container used to store a value
+number = 10
+firstname="Samantha"
+lastname="Kimani"
+language="python"
+a=10
+b=15
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(language,"is a web programming language")
+print(a+b)
+print("the sum of",a,"and",b,"is",a+b)
