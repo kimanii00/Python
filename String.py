73eaf30191389a5d432abbc960ff4f1166d5e589
kimannii00 <samanthakimani06@gmail.com>
2025-02-04 13:17:04 +0300

    second commit

---
+++ String.py
@@ -0,0 +1,23 @@
+text ="Hello World"
+language = "PYTHON"
+greeting ="Hello"
+name ="Samantha"
+space =" "
+print(text)
+print(language)
+
+#Accessing an element in a string
+print(text[0])
+print(text[1])
+
+#Size of a string
+print(len(text))
+
+#Modifying a string
+print(text.upper())
+print(language.lower())
+
+#String Concatenation-Joining strings
+print(greeting + name)
+print(greeting + " " + name)
+print(greeting + space + name)
---
+++ datatype.py
@@ -0,0 +1,31 @@
+number = 45 # This is an Integer
+num = 66.78 #This is a Float
+text = "Hello" # This is a String
+isMammal = True # Boolean
+
+# Data Structures
+cars = ["Toyota", "Mercedes", "Ford"] # List
+fruits = ("banana", "apple", "grape") # Tuple
+countries = {"England", "Italy", "Germany"} # Set
+details = {
+    "firstname": "Brian",
+    "course": "MIT",
+    "age": 19,
+    "maritalstatus": "Single"
+}
+
+
+print(number)
+print(num)
+print(text)
+print(isMammal)
+print(cars)
+print(fruits)
+print(countries)
+print(details)
+print(details["course"])
+
+# Typecasting
+
+print(float(number))
+print(int(num))
